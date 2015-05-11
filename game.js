e11fa45c8cb2af42d3c170cc9219bcb13b8043a2
Trey Tomlinson <tomlinson.trey@gmail.com>
2015-05-11 14:23:29 -0600

    created keyboarder module to handle keyboard input

--- game.js
+++ game.js
@@ -43,10 +43,30 @@
 
     }
   };
+
   var drawRect = function(screen, body){
     screen.fillRect(body.center.x - body.size.x / 2,
                     body.center.y - body.size.y / 2,
                     body.size.x, body.size.y);
+  };
+
+  var Keyboarder = function(){
+    var keyState = {};
+
+    window.onkeydown = function(e) {
+      keyState[e.keyCode] = true
+    };
+
+    window.onkeyup = function(e) {
+      keyState[e.keyCode] = false
+    };
+
+    this.isDown = function(keyCode) {
+      return keyState[keyCode] === true;
+    };
+
+    this.KEYS = { LEFT: 37, RIGHT: 39, SPACE: 32 };
+
   };
 
   window.onload = function() {
