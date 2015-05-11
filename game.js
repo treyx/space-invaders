5a9f36225384e3294bb0d4d53929c10bf2449079
Trey Tomlinson <tomlinson.trey@gmail.com>
2015-05-11 12:47:20 -0600

    fixed typo

--- game.js
+++ game.js
@@ -1,6 +1,6 @@
 ;(function() {
   var Game = function(canvasId) {
-    var canvas = document.getElementById(convasId) ;
+    var canvas = document.getElementById(canvasId) ;
     var screen = canvas.getContext("2d");
     var gameSize = { x: canvas.width, y: canvas.height };
 
