dc84f60130ba02708edc0beda2be06d216b590c3
Trey Tomlinson <tomlinson.trey@gmail.com>
2015-05-11 14:37:51 -0600

    removed a comment and added some player movement

--- game.js
+++ game.js
@@ -18,11 +18,13 @@
 
   Game.prototype = {
     update: function(){
+      for (var i = 0; i < this.bodies.length; i++) {
+        this.bodies[i].update();
+      }
 
     },
 
     draw: function(screen, gameSize) {
-      // screen.fillRect(30, 30, 40, 40);
       for (var i = 0; i < this.bodies.length; i++) {
         drawRect(screen, this.bodies[i]);
       }
@@ -36,10 +38,16 @@
     this.game = game;
     this.size = { x: 15, y: 15 };
     this.center = { x: gameSize.x / 2, y: gameSize.y - this.size.x };
+    this.keyboarder = new Keyboarder();
   };
 
   Player.prototype = {
     update: function(){
+      if (this.keyboarder.isDown(this.keyboarder.KEYS.LEFT)) {
+        this.center.x -= 2;
+      } else if (this.keyboarder.isDown(this.keyboarder.KEYS.RIGHT)) {
+        this.center.x += 2;
+      }
 
     }
   };
