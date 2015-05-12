4df2dd815c9b2709476080fbca63151c2653ac18
Trey Tomlinson <tomlinson.trey@gmail.com>
2015-05-11 21:23:20 -0600

    finished game

--- game.js
+++ game.js
@@ -1,140 +1,224 @@
 ;(function() {
-  var Game = function(canvasId) {
-    var canvas = document.getElementById(canvasId) ;
-    var screen = canvas.getContext("2d");
-    var gameSize = { x: canvas.width, y: canvas.height };
-
-    this.bodies = createInvaders(this).concat(new Player(this, gameSize));
+  var Game = function() {
+    var screen = document.getElementById("screen").getContext('2d');
+    this.size = { x: screen.canvas.width, y: screen.canvas.height };
+    this.bodies = createInvaders(this).concat(new Player(this));
 
     var self = this;
     var tick = function() {
       self.update();
-      self.draw(screen, gameSize);
+      self.draw(screen);
       requestAnimationFrame(tick);
     };
 
-    tick ();
+    tick();
   };
 
   Game.prototype = {
-    update: function(){
+    update: function() {
+      reportCollisions(this.bodies);
+
       for (var i = 0; i < this.bodies.length; i++) {
-        this.bodies[i].update();
+        if (this.bodies[i].update !== undefined) {
+          this.bodies[i].update();
+        }
       }
     },
 
-    draw: function(screen, gameSize) {
-      screen.clearRect(0, 0, gameSize.x, gameSize.y);
+    draw: function(screen) {
+      screen.clearRect(0, 0, this.size.x, this.size.y);
       for (var i = 0; i < this.bodies.length; i++) {
-        drawRect(screen, this.bodies[i]);
+        if (this.bodies[i].draw !== undefined) {
+          this.bodies[i].draw(screen);
+        }
       }
+    },
+
+    invadersBelow: function(invader) {
+      return this.bodies.filter(function(b) {
+        return b instanceof Invader &&
+          Math.abs(invader.center.x - b.center.x) < b.size.x &&
+          b.center.y > invader.center.y;
+      }).length > 0;
     },
 
     addBody: function(body) {
       this.bodies.push(body);
-    }
-  };
+    },
 
-  var Player = function(game, gameSize) {
-    this.game = game;
-    this.size = { x: 15, y: 15 };
-    this.center = { x: gameSize.x / 2, y: gameSize.y - this.size.x };
-    this.keyboarder = new Keyboarder();
-  };
-
-  Player.prototype = {
-    update: function(){
-      if (this.keyboarder.isDown(this.keyboarder.KEYS.LEFT)) {
-        this.center.x -= 2;
-      } else if (this.keyboarder.isDown(this.keyboarder.KEYS.RIGHT)) {
-        this.center.x += 2;
-      }
-      if (this.keyboarder.isDown(this.keyboarder.KEYS.SPACE)) {
-        var bullet = new Bullet({ x: this.center.x, y: this.center.y - this.size.x / 2 },
-                                {x: 0, y: -6 });
-        this.game.addBody(bullet);
+    removeBody: function(body) {
+      var bodyIndex = this.bodies.indexOf(body);
+      if (bodyIndex !== -1) {
+        this.bodies.splice(bodyIndex, 1);
       }
     }
   };
 
   var Invader = function(game, center) {
     this.game = game;
-    this.size = { x: 15, y: 15 };
     this.center = center;
+    this.size = { x: 15, y: 15 };
     this.patrolX = 0;
     this.speedX = 0.3;
   };
 
   Invader.prototype = {
-    update: function(){
-      if (this.patrolX < 0 || this.patrolX > 40) {
-        this.speedX = -this.speedX
+    update: function() {
+      if (this.patrolX < 0 || this.patrolX > 30) {
+        this.speedX = -this.speedX;
       }
+
+      if (Math.random() > 0.995 &&
+          !this.game.invadersBelow(this)) {
+        var bullet = new Bullet(this.game,
+                                { x: this.center.x, y: this.center.y + this.size.y / 2 },
+                                { x: Math.random() - 0.5, y: 2 });
+        this.game.addBody(bullet);
+      }
+
       this.center.x += this.speedX;
       this.patrolX += this.speedX;
+    },
+
+    draw: function(screen) {
+      drawRect(screen, this);
+    },
+
+    collision: function() {
+      this.game.removeBody(this);
     }
   };
 
   var createInvaders = function(game) {
     var invaders = [];
     for (var i = 0; i < 24; i++) {
-      var x = 30 + (i % 8) * 30;
-      var y = 30 + (i % 3) * 30;
-      invaders.push(new Invader(game, { x: x, y: y }));
-
-
+      var x = 35 + (i % 8) * 30;
+      var y = 35 + (i % 3) * 30;
+      invaders.push(new Invader(game, { x: x, y: y}));
     }
-    return invaders;
-  }
 
-  var Bullet = function(center, velocity) {
-    this.size = { x: 3, y: 3 };
+    return invaders;
+  };
+
+  var Player = function(game) {
+    this.game = game;
+    this.size = { x: 15, y: 15 };
+    this.center = { x: this.game.size.x / 2, y: this.game.size.y - 35 };
+    this.keyboarder = new Keyboarder();
+  };
+
+  Player.prototype = {
+    update: function() {
+      if (this.keyboarder.isDown(this.keyboarder.KEYS.LEFT)) {
+        this.center.x -= 2;
+      } else if (this.keyboarder.isDown(this.keyboarder.KEYS.RIGHT)) {
+        this.center.x += 2;
+      }
+
+      if (this.keyboarder.isDown(this.keyboarder.KEYS.SPACE)) {
+        var bullet = new Bullet(this.game,
+                                { x: this.center.x, y: this.center.y - this.size.y - 10 },
+                                { x: 0, y: -7 });
+        this.game.addBody(bullet);
+      }
+    },
+
+    draw: function(screen) {
+      drawRect(screen, this);
+    },
+
+    collision: function() {
+      this.game.removeBody(this);
+    }
+  };
+
+  var Bullet = function(game, center, velocity) {
+    this.game = game;
     this.center = center;
+    this.size = { x: 3, y: 3 };
     this.velocity = velocity;
   };
 
   Bullet.prototype = {
-    update: function(){
+    update: function() {
       this.center.x += this.velocity.x;
       this.center.y += this.velocity.y;
+
+      var screenRect = {
+        center: { x: this.game.size.x / 2, y: this.game.size.y / 2 },
+        size: this.game.size
+      };
+
+      if (!isColliding(this, screenRect)) {
+        this.game.removeBody(this);
+      }
+    },
+
+    draw: function(screen) {
+      drawRect(screen, this);
+    },
+
+    collision: function() {
+      this.game.removeBody(this);
     }
   };
 
-  var drawRect = function(screen, body){
-    screen.fillRect(body.center.x - body.size.x / 2,
-                    body.center.y - body.size.y / 2,
-                    body.size.x, body.size.y);
-  };
-
-  var Keyboarder = function(){
+  var Keyboarder = function() {
     var keyState = {};
+    window.addEventListener('keydown', function(e) {
+      keyState[e.keyCode] = true;
+    });
 
-    window.onkeydown = function(e) {
-      keyState[e.keyCode] = true
-    };
-
-    window.onkeyup = function(e) {
-      keyState[e.keyCode] = false
-    };
+    window.addEventListener('keyup', function(e) {
+      keyState[e.keyCode] = false;
+    });
 
     this.isDown = function(keyCode) {
       return keyState[keyCode] === true;
     };
 
     this.KEYS = { LEFT: 37, RIGHT: 39, SPACE: 32 };
-
   };
 
-  var colliding = function(b1, b2) {
-    return !(b1 === b2 ||
-             b1.center.x + b1.size.x / 2 < b2.center.x - b2.size.x / 2 ||
-             b1.center.y + b1.size.y / 2 < b2.center.y - b2.size.y / 2 ||
-             b1.center.x + b1.size.x / 2 > b2.center.x - b2.size.x / 2 ||
-             b1.center.y + b1.size.y / 2 > b2.center.y - b2.size.y / 2);
+  var drawRect = function(screen, body) {
+    screen.fillRect(body.center.x - body.size.x / 2,
+                    body.center.y - body.size.y / 2,
+                    body.size.x,
+                    body.size.y);
   };
 
-  window.onload = function() {
-    new Game("screen");
+  var isColliding = function(b1, b2) {
+    return !(
+      b1 === b2 ||
+        b1.center.x + b1.size.x / 2 <= b2.center.x - b2.size.x / 2 ||
+        b1.center.y + b1.size.y / 2 <= b2.center.y - b2.size.y / 2 ||
+        b1.center.x - b1.size.x / 2 >= b2.center.x + b2.size.x / 2 ||
+        b1.center.y - b1.size.y / 2 >= b2.center.y + b2.size.y / 2
+    );
   };
 
+  var reportCollisions = function(bodies) {
+    var bodyPairs = [];
+    for (var i = 0; i < bodies.length; i++) {
+      for (var j = i + 1; j < bodies.length; j++) {
+        if (isColliding(bodies[i], bodies[j])) {
+          bodyPairs.push([bodies[i], bodies[j]]);
+        }
+      }
+    }
+
+    for (var i = 0; i < bodyPairs.length; i++) {
+      if (bodyPairs[i][0].collision !== undefined) {
+        bodyPairs[i][0].collision(bodyPairs[i][1]);
+      }
+
+      if (bodyPairs[i][1].collision !== undefined) {
+        bodyPairs[i][1].collision(bodyPairs[i][0]);
+      }
+    }
+  };
+
+  window.addEventListener('load', function() {
+    new Game();
+  });
 })();
